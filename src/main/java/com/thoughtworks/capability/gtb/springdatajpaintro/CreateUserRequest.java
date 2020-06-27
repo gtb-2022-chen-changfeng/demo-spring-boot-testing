a86605715f6ead37fdc99eaaaf00cfa9d2554555
Xiaofeng Wang <xfun68@gmail.com>
2020-06-27 22:39:38 +0800

    implement basic user operations

--- src/main/java/com/thoughtworks/capability/gtb/springdatajpaintro/CreateUserRequest.java
+++ src/main/java/com/thoughtworks/capability/gtb/springdatajpaintro/CreateUserRequest.java
@@ -3,8 +3,7 @@ package com.thoughtworks.capability.gtb.springdatajpaintro;
 import lombok.Data;
 
 @Data
-public class User {
-    private Long id;
+public class CreateUserRequest {
     private String name;
     private Long age;
     private String avatar;
---
+++ src/main/java/com/thoughtworks/capability/gtb/springdatajpaintro/UserController.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.capability.gtb.springdatajpaintro;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.findUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found"));
+    }
+
+    @PostMapping("/users")
+    public Long createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+}
