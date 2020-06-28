bbae7294f2300b2e24a057c184973ed14203cc0e
Xiaofeng Wang <xfun68@gmail.com>
2020-06-28 18:52:51 +0800

    no need to extend from ResponseEntityExceptionHandler

--- src/main/java/com/thoughtworks/capability/gtb/springdatajpaintro/ExceptionHandler.java
+++ src/main/java/com/thoughtworks/capability/gtb/springdatajpaintro/ExceptionHandler.java
@@ -4,10 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(UserNotExistedException.class)
     public ResponseEntity<ApiError> handleNotExistedUser(UserNotExistedException ex, WebRequest webRequest) {
