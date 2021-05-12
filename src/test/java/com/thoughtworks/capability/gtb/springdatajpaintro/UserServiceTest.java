0d06a65ca0e2164e4cd04becbb253d648c58323d
fxzou <vvvzfx@gmail.com>
2021-05-12 15:58:08 +0800

    add unit test demo

--- src/test/java/com/thoughtworks/capability/gtb/springdatajpaintro/UserServiceTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/springdatajpaintro/UserServiceTest.java
@@ -1,16 +1,19 @@
 package com.thoughtworks.capability.gtb.springdatajpaintro;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -69,5 +72,35 @@ public class UserServiceTest {
                 assertThat(thrownException.getMessage()).containsSequence("User Not Found");
             }
         }
+    }
+
+    @Nested
+    class CreateUser {
+
+        @Test
+        public void should_save_user_in_repository_when_create_user() {
+            // given
+            UserRepository userRepository = Mockito.mock(UserRepository.class);
+            Mockito.when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
+                User userArg = (User) invocation.getArguments()[0];
+                userArg.setId(123L);
+                return userArg;
+            });
+            UserService userService = new UserService(userRepository);
+            User newUser = User.builder()
+                    .name("Panda")
+                    .age(24L)
+                    .avatar("http://...")
+                    .description("A good guy.")
+                    .build();
+
+            // when
+            Long userId = userService.createUser(newUser);
+
+            // then
+            Assertions.assertThat(userId).isEqualTo(123L);
+            Mockito.verify(userRepository).save(newUser);
+        }
+
     }
 }
