984513772d33f01dab31ed5a503f6b8bfef54e80
fxzou <vvvzfx@gmail.com>
2021-05-18 16:12:05 +0800

    add find all user jpa test

--- src/test/java/com/thoughtworks/capability/gtb/springdatajpaintro/UserRepositoryTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/springdatajpaintro/UserRepositoryTest.java
@@ -1,17 +1,22 @@
 package com.thoughtworks.capability.gtb.springdatajpaintro;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
+@Rollback
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
@@ -20,22 +25,43 @@ public class UserRepositoryTest {
 
     @Test
     void should_return_user_when_id_exists() {
-        entityManager.persistAndFlush(User.builder()
-                .name("Panda")
+        User savedUser = User.builder()
+                .name("Panda1")
                 .age(24L)
                 .avatar("http://...")
                 .description("A good guy.")
-                .build());
+                .build();
+        entityManager.persistAndFlush(savedUser);
 
-        Optional<User> found = userRepository.findOneById(1L);
+        Optional<User> found = userRepository.findOneById(savedUser.getId());
 
         assertThat(found.isPresent()).isTrue();
         assertThat(found.get()).isEqualTo(User.builder()
-                .id(1L)
-                .name("Panda")
+                .id(savedUser.getId())
+                .name("Panda1")
                 .age(24L)
                 .avatar("http://...")
                 .description("A good guy.")
                 .build());
+    }
+
+    @Test
+    void should_return_user_list() {
+        // given
+        User user = User.builder()
+                .name("Panda2")
+                .age(24L)
+                .avatar("http://...")
+                .description("A good guy.")
+                .educations(Collections.emptyList())
+                .build();
+        User savedUser = entityManager.persist(user);
+
+        // when
+        List<User> userList = userRepository.findAll(); // size: 2 ? size: 1
+
+        // then
+        Assertions.assertThat(userList).containsOnly(savedUser);
+
     }
 }
